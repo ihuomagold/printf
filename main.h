08613bf5923320b049f60f5d2f592952a2dc8adf
Ihuoma Ogbuji <ihuomagold53@gmail.com>
2022-08-08 01:27:37 -0700

    added task 1 and converter files

--- main.h
+++ main.h
@@ -1,6 +1,51 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdlib.h>
+#include <stdarg.h>
+
+/**
+ * struct flags - struct containing flags to "turn on"
+ * when a flag specifier is passed to _printf()
+ * @plus: flag for the '+' character
+ * @space: flag for the ' ' character
+ * @hash: flag for the '#' character
+ */
+
+typedef struct flags
+{
+	int plus;
+	int space;
+	int hash;
+} flags_t;
+
+/**
+ * struct printHandler - struct to choose the right function depending
+ * on the format specifier passed to _printf()
+ * @c: format specifier
+ * @f: pointer to the correct printing function
+ */
+
+typedef struct printHandler
+{
+	char c;
+	int (*f)(va_list ap, flags_t *f);
+} ph;
+
+/* putchar and puts function*/
+int _putchar(char c);
+int _puts(char *str);
+
+/* converter */
+char *convert(unsigned long int num, int base, int lowercase);
+
+/* for printf */
 int _printf(const char *format, ...);
+
+/* for printnum */
+int print_int(va_list l, flags_t *f);
+void print_number(int n);
+int print_unsigned(va_list l, flags_t *f);
+int count_digit(int i);
 
 #endif /* MAIN_H */
